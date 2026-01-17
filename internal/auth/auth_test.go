d00a285d0024bb020e3e7311c78fe04dfe587380
Erik Nytting <erik.nytting@tiqqe.com>
2026-01-17 16:31:55 +0800

    adding tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,43 @@
+package auth_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	t.Run("missing header", func(t *testing.T) {
+		headers := http.Header{}
+		_, err := auth.GetAPIKey(headers)
+		if err == nil {
+			t.Errorf("expected error, got nil")
+		}
+	})
+
+	t.Run("malformed auth header", func(t *testing.T) {
+		headers := http.Header{
+			"Authorization": []string{"NotCorrectHeader"},
+		}
+		_, err := auth.GetAPIKey(headers)
+		if err == nil {
+			t.Errorf("expected error, got nil")
+		}
+	})
+
+	t.Run("correct header", func(t *testing.T) {
+		headers := http.Header{
+			"Authorization": []string{"ApiKeys some-key"},
+		}
+		value, err := auth.GetAPIKey(headers)
+		if err != nil {
+			t.Errorf("expected no error, got: %s", err)
+			return
+		}
+
+		if value != "some-key" {
+			t.Errorf("wrong api key, got=%s", value)
+		}
+	})
+}
